74260ce46a189418c07cdf9cfbc7f654d0c91dce
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-15 20:13:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 
 }
 function deleteFromObjectByKey(object,key) {
-  return Object.assign({}, object,{[key]: value})
+  delete object.'key'
 }
