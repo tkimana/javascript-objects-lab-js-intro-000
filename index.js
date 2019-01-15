fcf844b0cf258969f5b54150e713c60f5ab55b41
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-15 19:16:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function updateObjectWithKeyAndValue(object,key,value) {
   return Object.assign({}, object, {[key]: value})
 
 }
+function destructivelyUpdateObjectwithKeyAndValue(object,key,value) {
+  object[key] = value
+  return object
+}
