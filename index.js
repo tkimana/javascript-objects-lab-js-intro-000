91e6eb8f5dfad2fd5308ae891825dd9362dd0bc3
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-15 19:17:40 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-var recipes = {sandwich: "egg"}
-function updateObjectWithKeyAndValue(object,key,value) {
-  return Object.assign({}, object, {[key]: value})
-
-}
-function destructivelyUpdateObjectwithKeyAndValue(object,key,value) {
-  object[key] = value
-  return object
-}
